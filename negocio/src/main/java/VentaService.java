4e798dc156830135caa2e86c61529c22379305f4
rosariogt <32083304+rosariogt@users.noreply.github.com>
2024-01-30 15:15:38 -0400

    Update VentaService.java

--- negocio/src/main/java/VentaService.java
+++ negocio/src/main/java/VentaService.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class VentaService {
 
-    public void registrar(NotaVenta notaVenta) {
+    public RespuestaRegistroVenta registrar(NotaVenta notaVenta) {
+        RespuestaRegistroVenta respuesta = new RespuestaRegistroVenta();
+        var mensajes = verificarParametricas(notaVenta);
+        if (!mensajes.isEmpty()) {
+            respuesta.setMensajes(mensajes);
+            return respuesta;
+        }
 
+        return null;
+    }
+
+    private List<Mensaje> verificarParametricas(NotaVenta notaVenta) {
+        List<Mensaje> mensajes = new ArrayList<>();
+        if (enums.EnumTipoPago.getCodigos().contains(notaVenta.getTipoPago()))
+            mensajes.add(new Mensaje(1, "NO EXISTE TIPO DE PAGO"));
+        if (enums.EnumTipoEntrega.getCodigos().contains(notaVenta.getTipoEntrega()))
+            mensajes.add(new Mensaje(2, "NO EXISTE TIPO DE ENTREGA"));
+        return mensajes;
     }
 
 }
