00c6dcf4436f2782437df88f054a1103c58008d9
rosariogt <32083304+rosariogt@users.noreply.github.com>
2024-01-30 15:15:15 -0400

    Update ProductoService.java

--- negocio/src/main/java/ProductoService.java
+++ negocio/src/main/java/ProductoService.java
@@ -2,8 +2,25 @@ import java.util.List;
 
 public class ProductoService {
 
-    private List<DetalleVenta> obtenerProductos() {
-        return null;
+   private Map<String, Producto> productosMap = new HashMap<>();
+    
+   public ProductoService() {
+        this.productosMap.put("01", new Producto("01", "tela", "producto", "metros"));
+        this.productosMap.put("02", new Producto("02", "pintura", "producto", "litros"));
+        this.productosMap.put("03", new Producto("03", "barniz", "producto", "litros"));
+        this.productosMap.put("04", new Producto("0", "pintados", "servicio", "dias"));
+    }
+
+    public Map<String, Producto> getProductosMap() {
+        return productosMap;
+    }
+
+    public void setProductosMap(Map<String, Producto> productosMap) {
+        this.productosMap = productosMap;
+    }
+
+    public List<Producto> obtenerProductos() {
+        return productosMap.values().stream().collect(Collectors.toList());
     }
 
 }
